b0924ee5dd60a1a16ad788c7f57d0005397df9ed
樺 <xd_301@yahoo.com.tw>
2022-12-08 10:18:46 +0800

    v1.2 設定搜尋路由 動態路由 ，套用json資料到 index 及 show

--- app.js
+++ app.js
@@ -6,16 +6,34 @@ const app = express();
 const port = 3000;
 //設定express-handlebars，存在exphbs的變數中
 const exphbs = require("express-handlebars");
-const restaurantlist = require("./restaurant.json");
+const restaurantList = require("./restaurant.json");
 //設置模板引擎，使用express-handlebars，預設布局main
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 //設定view engine裡的檔案為handlebars
 app.set("view engine", "handlebars");
 //設置靜態文件
-app.use(express.static('public'))
+app.use(express.static("public"));
 //設定路由器
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render("index", { restaurants: restaurantList.results });
+});
+//設定搜尋路由，搜尋符合餐廳名字或類別
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return (
+      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    );
+  });
+  res.render("index", { restaurants: restaurants, keyword: keyword });
+});
+//設定動態路由，找出相對id的餐廳資訊
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
 });
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
